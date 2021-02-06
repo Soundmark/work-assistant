e5e7ab51aa26855cb427c9cf0caa0c503dea69c1
郑家豪 <zhengjh@tsintergy.com>
2021-02-06 18:24:56 +0800

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,30 @@
+import { Button, Layout, Menu } from 'antd';
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import icon from '../assets/icon.svg';
-import './App.global.css';
+import ColorPicker from './pages/ColorPicker/index';
+import 'antd/dist/antd.css';
 
-const Hello = () => {
-  return (
-    <div>
-      <div className="Hello">
-        <img width="200px" alt="icon" src={icon} />
-      </div>
-      <h1>electron-react-boilerplate</h1>
-      <div className="Hello">
-        <a
-          href="https://electron-react-boilerplate.js.org/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              📚
-            </span>
-            Read our docs
-          </button>
-        </a>
-        <a
-          href="https://github.com/sponsors/electron-react-boilerplate"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              🙏
-            </span>
-            Donate
-          </button>
-        </a>
-      </div>
-    </div>
-  );
-};
+const { Sider } = Layout;
+const { SubMenu } = Menu;
 
 export default function App() {
   return (
     <Router>
+      <Layout>
+        <Sider>
+          <Menu mode="horizontal">
+            {/* <SubMenu> */}
+            <Menu.Item key="1">option1</Menu.Item>
+            <Menu.Item key="2">option2</Menu.Item>
+            <Menu.Item key="3">
+              <Button>222</Button>
+            </Menu.Item>
+            {/* </SubMenu> */}
+          </Menu>
+        </Sider>
+      </Layout>
       <Switch>
-        <Route path="/" component={Hello} />
+        <Route path="/" component={ColorPicker} />
       </Switch>
     </Router>
   );
---
+++ src/pages/ColorPicker/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function ColorPicker() {
+  return 123;
+}
+
+export default ColorPicker;
