1d6c6c8d7452f8b8e48fb830cded26c27858b77d
Soundmark <beatboxzjh@163.com>
2021-02-19 23:02:34 +0800

    ci: eslint配置修改

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,8 @@ module.exports = {
     'jsx-a11y/no-static-element-interactions': 'off',
     'jsx-a11y/click-events-have-key-events': 'off',
     'no-console': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
   },
   parserOptions: {
     ecmaVersion: 2020,
